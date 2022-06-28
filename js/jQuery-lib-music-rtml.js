f2ffe3346e797da491b820f70b9d6f9a6a617ef6
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2022-06-28 21:05:43 +0700

    Add files via upload

---
+++ js/jQuery-lib-music-rtml.js
@@ -0,0 +1,378 @@
+/* Copyright 2017
+by iyortml
+*/
+
+function rtmlAKU() {
+	window.location.assign("https://sites.google.com/site/iyortml/home/Akan%20Ku%20Usahakan.mp3?attredirects=0&d=1");
+}
+
+function rtmlB() {
+	window.location.assign("https://sites.google.com/site/radiotipmasalampau/disc-6-semester-1-lagi/Radio%20Tip%20Masa%20Lampau%20-%20Bersembunyi.mp3?attredirects=0&d=1");
+}
+
+function rtmlBH() {
+	window.location.assign("https://sites.google.com/site/radiotipmasalampau/disc-4-bisikan-hati/Radio%20Tip%20Masa%20Lampau%20-%20Bisikan%20Hati.mp3?attredirects=0&d=1");
+}
+
+function rtmlCSKM() {
+	window.location.assign("https://sites.google.com/site/iyortml/home/Radio%20Tip%20Masa%20Lampau%20-%20Cinta%20SejatiKu%20Kini%20MilikNya.mp3?attredirects=0&d=1");
+}
+
+function rtmlCL() {
+	window.location.assign("https://sites.google.com/site/radiotipmasalampau/disc-6-semester-1-lagi/Radio%20Tip%20Masa%20Lampau%20-%20CinLok.mp3?attredirects=0&d=1");
+}
+
+function rtmlDAK() {
+	window.location.assign("https://sites.google.com/site/radiotipmasalampau/disc-5-peran-yang-sama/Radio%20Tip%20Masa%20Lampau%20-%20Di%20Antara%20Kita.mp3?attredirects=0&d=1");
+}
+
+function rtmlDTJDH() {
+	window.location.assign("http://ghostdrivers.freeiz.com/iyortml/music/rtml04/Radio%20Tip%20Masa%20Lampau%20-%20Dirimu%20Telah%20Jatuh%20Di%20HatiKu.mp3");
+}
+
+function rtmlFRF() {
+	window.location.assign("https://sites.google.com/site/iyortml/home/IyoRTML%20Feat%20Raka%20F%20-%20100611.mp3?attredirects=0&d=1");
+}
+
+function rtmlGHK() {
+	window.location.assign("https://sites.google.com/site/iyortml/home/Radio%20Tip%20Masa%20Lampau%20-%20Gelap%20HariKu.mp3?attredirects=0&d=1");
+}
+
+function rtmlHM() {
+	window.location.assign("http://ghostdrivers.freeiz.com/iyortml/music/rtml06/Radio%20Tip%20Masa%20Lampau%20-%20Hanya%20Mimpi.mp3");
+}
+
+function rtmlKPSM() {
+	window.location.assign("https://sites.google.com/site/iyortml/home/Radio%20Tip%20Masa%20Lampau%20-%20Ku%20Pasti%20SepertiMu.mp3?attredirects=0&d=1");
+}
+
+function rtmlMKM() {
+	window.location.assign("https://sites.google.com/site/iyortml/home/Radio%20Tip%20Masa%20Lampau%20-%20Maaf%20Ku%20MengejarMu.mp3?attredirects=0&d=1");
+}
+
+function rtmlMRR() {
+	window.location.assign("https://sites.google.com/site/iyortml/home/Radio%20Tip%20Masa%20Lampau%20-%20Mendalami%20Rasa%20%28REMIX%29.mp3?attredirects=0&d=1");
+}
+
+function rtmlSUN() {
+	window.location.assign("https://sites.google.com/site/iyortml/home/Radio%20Tip%20Masa%20Lampau%20-%20SenyumMu%20UntukNya.mp3?attredirects=0&d=1");
+}
+
+function rtmlTDH() {
+	window.location.assign("https://sites.google.com/site/radiotipmasalampau/disc-3-kita-berdua-untuk-selamanya/Radio%20Tip%20Masa%20Lampau%20-%20Tetap%20Di%20Hati.mp3?attredirects=0&d=1");
+}
+
+/* lirik 1 */
+$(document).ready(function(){
+	$("#click1").click(function(){
+		$("#pane1").slideToggle()
+		$("#pane2").hide()
+		$("#pane3").hide()
+		$("#pane4").hide()
+		$("#pane5").hide()
+		$("#pane6").hide()
+		$("#pane7").hide()
+		$("#pane8").hide()
+		$("#pane9").hide()
+		$("#pane10").hide()
+		$("#pane11").hide()
+		$("#pane12").hide()
+		$("#pane13").hide()
+		$("#pane14").hide()
+		$("#pane15").hide()
+	});
+});
+
+/* lirik 2 */
+$(document).ready(function(){
+	$("#click2").click(function(){
+		$("#pane1").hide()
+		$("#pane2").slideToggle()
+		$("#pane3").hide()
+		$("#pane4").hide()
+		$("#pane5").hide()
+		$("#pane6").hide()
+		$("#pane7").hide()
+		$("#pane8").hide()
+		$("#pane9").hide()
+		$("#pane10").hide()
+		$("#pane11").hide()
+		$("#pane12").hide()
+		$("#pane13").hide()
+		$("#pane14").hide()
+		$("#pane15").hide()
+	});
+});
+
+/* lirik 3 */
+$(document).ready(function(){
+	$("#click3").click(function(){
+		$("#pane1").hide()
+		$("#pane2").hide()
+		$("#pane3").slideToggle()
+		$("#pane4").hide()
+		$("#pane5").hide()
+		$("#pane6").hide()
+		$("#pane7").hide()
+		$("#pane8").hide()
+		$("#pane9").hide()
+		$("#pane10").hide()
+		$("#pane11").hide()
+		$("#pane12").hide()
+		$("#pane13").hide()
+		$("#pane14").hide()
+		$("#pane15").hide()
+	});
+});
+
+/* lirik 4 */
+$(document).ready(function(){
+	$("#click4").click(function(){
+		$("#pane1").hide()
+		$("#pane2").hide()
+		$("#pane3").hide()
+		$("#pane4").slideToggle()
+		$("#pane5").hide()
+		$("#pane6").hide()
+		$("#pane7").hide()
+		$("#pane8").hide()
+		$("#pane9").hide()
+		$("#pane10").hide()
+		$("#pane11").hide()
+		$("#pane12").hide()
+		$("#pane13").hide()
+		$("#pane14").hide()
+		$("#pane15").hide()
+	});
+});
+
+/* lirik 5 */
+$(document).ready(function(){
+	$("#click5").click(function(){
+		$("#pane1").hide()
+		$("#pane2").hide()
+		$("#pane3").hide()
+		$("#pane4").hide()
+		$("#pane5").slideToggle()
+		$("#pane6").hide()
+		$("#pane7").hide()
+		$("#pane8").hide()
+		$("#pane9").hide()
+		$("#pane10").hide()
+		$("#pane11").hide()
+		$("#pane12").hide()
+		$("#pane13").hide()
+		$("#pane14").hide()
+		$("#pane15").hide()
+	});
+});
+
+/* lirik 6 */
+$(document).ready(function(){
+	$("#click6").click(function(){
+		$("#pane1").hide()
+		$("#pane2").hide()
+		$("#pane3").hide()
+		$("#pane4").hide()
+		$("#pane5").hide()
+		$("#pane6").slideToggle()
+		$("#pane7").hide()
+		$("#pane8").hide()
+		$("#pane9").hide()
+		$("#pane10").hide()
+		$("#pane11").hide()
+		$("#pane12").hide()
+		$("#pane13").hide()
+		$("#pane14").hide()
+		$("#pane15").hide()
+	});
+});
+
+/* lirik 7 */
+$(document).ready(function(){
+	$("#click7").click(function(){
+		$("#pane1").hide()
+		$("#pane2").hide()
+		$("#pane3").hide()
+		$("#pane4").hide()
+		$("#pane5").hide()
+		$("#pane6").hide()
+		$("#pane7").slideToggle()
+		$("#pane8").hide()
+		$("#pane9").hide()
+		$("#pane10").hide()
+		$("#pane11").hide()
+		$("#pane12").hide()
+		$("#pane13").hide()
+		$("#pane14").hide()
+		$("#pane15").hide()
+	});
+});
+
+/* lirik 8 */
+$(document).ready(function(){
+	$("#click8").click(function(){
+		$("#pane1").hide()
+		$("#pane2").hide()
+		$("#pane3").hide()
+		$("#pane4").hide()
+		$("#pane5").hide()
+		$("#pane6").hide()
+		$("#pane7").hide()
+		$("#pane8").slideToggle()
+		$("#pane9").hide()
+		$("#pane10").hide()
+		$("#pane11").hide()
+		$("#pane12").hide()
+		$("#pane13").hide()
+		$("#pane14").hide()
+		$("#pane15").hide()
+	});
+});
+
+/* lirik 9 */
+$(document).ready(function(){
+	$("#click9").click(function(){
+		$("#pane1").hide()
+		$("#pane2").hide()
+		$("#pane3").hide()
+		$("#pane4").hide()
+		$("#pane5").hide()
+		$("#pane6").hide()
+		$("#pane7").hide()
+		$("#pane8").hide()
+		$("#pane9").slideToggle()
+		$("#pane10").hide()
+		$("#pane11").hide()
+		$("#pane12").hide()
+		$("#pane13").hide()
+		$("#pane14").hide()
+		$("#pane15").hide()
+	});
+});
+
+/* lirik 10 */
+$(document).ready(function(){
+	$("#click10").click(function(){
+		$("#pane1").hide()
+		$("#pane2").hide()
+		$("#pane3").hide()
+		$("#pane4").hide()
+		$("#pane5").hide()
+		$("#pane6").hide()
+		$("#pane7").hide()
+		$("#pane8").hide()
+		$("#pane9").hide()
+		$("#pane10").slideToggle()
+		$("#pane11").hide()
+		$("#pane12").hide()
+		$("#pane13").hide()
+		$("#pane14").hide()
+		$("#pane15").hide()
+	});
+});
+
+/* lirik 11 */
+$(document).ready(function(){
+	$("#click11").click(function(){
+		$("#pane1").hide()
+		$("#pane2").hide()
+		$("#pane3").hide()
+		$("#pane4").hide()
+		$("#pane5").hide()
+		$("#pane6").hide()
+		$("#pane7").hide()
+		$("#pane8").hide()
+		$("#pane9").hide()
+		$("#pane10").hide()
+		$("#pane11").slideToggle()
+		$("#pane12").hide()
+		$("#pane13").hide()
+		$("#pane14").hide()
+		$("#pane15").hide()
+	});
+});
+
+/* lirik 12 */
+$(document).ready(function(){
+	$("#click12").click(function(){
+		$("#pane1").hide()
+		$("#pane2").hide()
+		$("#pane3").hide()
+		$("#pane4").hide()
+		$("#pane5").hide()
+		$("#pane6").hide()
+		$("#pane7").hide()
+		$("#pane8").hide()
+		$("#pane9").hide()
+		$("#pane10").hide()
+		$("#pane11").hide()
+		$("#pane12").slideToggle()
+		$("#pane13").hide()
+		$("#pane14").hide()
+		$("#pane15").hide()
+	});
+});
+
+/* lirik 13 */
+$(document).ready(function(){
+	$("#click13").click(function(){
+		$("#pane1").hide()
+		$("#pane2").hide()
+		$("#pane3").hide()
+		$("#pane4").hide()
+		$("#pane5").hide()
+		$("#pane6").hide()
+		$("#pane7").hide()
+		$("#pane8").hide()
+		$("#pane9").hide()
+		$("#pane10").hide()
+		$("#pane11").hide()
+		$("#pane12").hide()
+		$("#pane13").slideToggle()
+		$("#pane14").hide()
+		$("#pane15").hide()
+	});
+});
+
+/* lirik 14 */
+$(document).ready(function(){
+	$("#click14").click(function(){
+		$("#pane1").hide()
+		$("#pane2").hide()
+		$("#pane3").hide()
+		$("#pane4").hide()
+		$("#pane5").hide()
+		$("#pane6").hide()
+		$("#pane7").hide()
+		$("#pane8").hide()
+		$("#pane9").hide()
+		$("#pane10").hide()
+		$("#pane11").hide()
+		$("#pane12").hide()
+		$("#pane13").hide()
+		$("#pane14").slideToggle()
+		$("#pane15").hide()
+	});
+});
+
+/* lirik 15 */
+$(document).ready(function(){
+	$("#click15").click(function(){
+		$("#pane1").hide()
+		$("#pane2").hide()
+		$("#pane3").hide()
+		$("#pane4").hide()
+		$("#pane5").hide()
+		$("#pane6").hide()
+		$("#pane7").hide()
+		$("#pane8").hide()
+		$("#pane9").hide()
+		$("#pane10").hide()
+		$("#pane11").hide()
+		$("#pane12").hide()
+		$("#pane13").hide()
+		$("#pane14").hide()
+		$("#pane15").slideToggle()
+	});
+});
